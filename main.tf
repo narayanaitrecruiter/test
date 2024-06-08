73005f729072e7ae275ae38a53155ef987415fba
narayana <narayanaitrecruiter@gmail.com>
2024-06-08 18:10:07 +0530

    updated

--- main.tf
+++ main.tf
@@ -7,17 +7,26 @@ module "ecs" {
   source    = "./modules/ecs"
   vpc_id    = module.vpc.vpc_id
   subnet_ids = module.vpc.subnet_ids
+
+  
 }
 
 module "eks" {
   source    = "./modules/eks"
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.vpc.subnet_ids
-
 }
 
 module "rds" {
   source    = "./modules/rds"
   vpc_id    = module.vpc.vpc_id
   subnet_ids = module.vpc.subnet_ids
+}
+
+module "lambda" {
+  source    = "./modules/lambda"
+}
+
+module "ecr" {
+  source    = "./modules/ecr"
 }

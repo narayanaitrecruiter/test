d7190c6ad68f3110ddc2311dc68acc4f478a5998
narayana <narayanaitrecruiter@gmail.com>
2024-06-09 20:18:08 +0530

    updated

--- modules/ecs/ecs_service.tf
+++ modules/ecs/ecs_service.tf
@@ -79,3 +79,48 @@ resource "aws_ecs_task_definition" "default" {
   }
 }
 
+
+resource "aws_ecs_task_definition" "default1" {
+  family             = var.task_definition_family1
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_iam_role.arn
+  container_definitions = file(var.container_definitions_file1)
+
+  tags = {
+    name =  var.environment
+  }
+}
+
+resource "aws_ecs_service" "service" {
+  name                               = "${var.namespace}_ECS_Service_${var.task_definition_family1}"
+  iam_role                           = aws_iam_role.ecs_service_role.arn
+  cluster                            = var.cluster_id
+  task_definition                    = aws_ecs_task_definition.default1.arn
+  desired_count                      = var.ecs_task_desired_count
+  deployment_minimum_healthy_percent = var.ecs_task_deployment_minimum_healthy_percent
+  deployment_maximum_percent         = var.ecs_task_deployment_maximum_percent
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.service_target_group.arn
+    container_name   = var.task_definition_family1
+    container_port   = var.container_port
+  }
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "memory"
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+
+  tags = {
+    name = var.environment
+  }
+}
